7f509ef4dc1ae9341cbf00fc96e3fd40707c2d3b
Fredrik Averpil <fredrik.averpil@gmail.com>
2025-03-08 19:47:14 +0100

    fix: handle import paths as well as symbols

--- lua/godoc/adapters/go.lua
+++ lua/godoc/adapters/go.lua
@@ -96,14 +96,55 @@ local function goto_package_definition(package, picker_gotodef_fun)
 	end
 
 	-- write temp file
-	local content = {
-		"package main",
-		"",
-		'import "' .. package .. '"',
-	}
-	local tempdir = vim.fn.tempname()
-	vim.fn.mkdir(tempdir, "p")
-	local tempfile = tempdir .. "/temp.go"
+	local content = {}
+	local cursor_pos = {}
+	local is_symbol = package:find("%.") ~= nil
+	if is_symbol then
+		-- an import with symbol is passed, e.g. archive/tar.FileInfoNames
+		--
+		-- extract the import path (archive/tar)
+		local import_path = package:match("^(.-)%.")
+		-- extract the package name, which comes after the last slash (e.g. tar)
+		local package_name = import_path:match("([^/]+)$")
+		-- extract the symbol name, which comes after the last dot (e.g. FileInfoNames)
+		local symbol = package:match("([^%.]+)$")
+		-- the contents of the go file, which contains imports to the package and the symbol as well as the fmt package
+		local line = '  fmt.Printf("%v", ' .. package_name .. "." .. symbol .. ")"
+		content = {
+			"package main",
+			"",
+			"import (",
+			'  "' .. import_path .. '"',
+			'  "fmt"',
+			")",
+			"",
+			"func main() {",
+			line,
+			"}",
+		}
+		-- put the position/cursor on the symbol, e.g. on the 'F' of FileInfoNames
+		cursor_pos = { 9, line:find(symbol) + 1 }
+	else
+		-- a package name is passed, e.g. "archive/tar"
+		local line = 'import "' .. package .. '"'
+		content = {
+			"package main",
+			"",
+			line,
+		}
+		cursor_pos = { 3, line:find(package) + 1 }
+	end
+	vim.notify(vim.inspect(cursor_pos))
+	local now = os.time()
+	local filename = "godoc_" .. now .. ".go"
+	local tempfile = vim.fn.getcwd() .. "/" .. filename
+	local go_mod_filepath = vim.fn.findfile("go.mod", ".;")
+	local go_mod_dir = vim.fn.fnamemodify(go_mod_filepath, ":p:h")
+	if go_mod_dir == "" then
+		vim.notify("Failed to find go.mod file, can only gotodef on std lib", vim.log.levels.WARN)
+	else
+		tempfile = go_mod_dir .. "/" .. filename
+	end
 	vim.fn.writefile(content, tempfile)
 
 	-- create hidden window to run picker in
@@ -122,6 +163,9 @@ local function goto_package_definition(package, picker_gotodef_fun)
 	-- get ahold of the created buffer
 	local buf = vim.api.nvim_win_get_buf(window)
 
+	-- disable diagnostics for the buffer
+	vim.diagnostic.enable(false, { bufnr = buf })
+
 	-- wait until LSP has attached, can be queried and returns a client_id
 	local client_id = nil
 	local maxretries = 50
@@ -136,15 +180,16 @@ local function goto_package_definition(package, picker_gotodef_fun)
 
 	-- execute in window
 	vim.api.nvim_win_call(window, function()
-		vim.api.nvim_win_set_cursor(0, { 3, 8 }) -- position cursor over package name
+		vim.api.nvim_win_set_cursor(0, cursor_pos) -- position cursor over package name
 		picker_gotodef_fun() -- run picker's goto definition function
 	end)
 
-	-- close hidden window and delete temp file, but wait so that picker has time to finish
+	--	close hidden window and delete temp file, but wait so that picker has time to finish
+	--	TODO: would be nice to do this more reliably, and not based on a timeout
 	vim.defer_fn(function()
 		vim.api.nvim_win_close(window, true)
 		vim.fn.delete(tempfile)
-	end, 500)
+	end, 2000)
 end
 
 local function health()
